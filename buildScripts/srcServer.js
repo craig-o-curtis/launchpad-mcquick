6431e6c62bb5be2b2c4948c6fc72f18e798d304a
Craig Curtis <ccurtis@unicon.net>
2017-06-17 20:39:18 -0700

    Convert srcServer to es6 syntax

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+//// es5 way
+// var express = require('express');
+// var path = require('path');
+// var open = require('open');
 
-var port = 3000;
-var app = express();
-var rootFile = '../src/index.html';
+const port = 3000;
+const app = express();
+const rootFile = '../src/index.html';
 
 app.get('/', function(req, res){
   res.sendFile( path.join(__dirname, rootFile ) );
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,5 +2,3 @@ import chalk from 'chalk'; // es6
 // var chalk = require('chalk'); // es5
 
 console.log(chalk.green('Starting app in dev mode...'));
-
-
